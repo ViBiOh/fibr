128687cab9a013244a97aaa39481da67a8cc7914
Vincent Boutour <vboutour@meilleursagents.com>
2018-01-18 09:00:51 +0100

    Adding method in form and changing color for display modifiers

--- crud/post.go
+++ crud/post.go
@@ -1,6 +1,7 @@
 package crud
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/ViBiOh/fibr/provider"
@@ -10,20 +11,13 @@ const maxPostMemory = 32 * 1024 * 2014 // 32 MB
 
 // Post handle post from form
 func (a *App) Post(w http.ResponseWriter, r *http.Request, config *provider.RequestConfig) {
-	isMultipart := false
-	isForm := false
+	r.ParseMultipartForm(maxPostMemory)
 
-	if r.ParseMultipartForm(maxPostMemory) == nil {
-		isMultipart = true
-	}
-
-	if r.ParseForm() == nil {
-		isForm = true
-	}
-
-	if isForm && !isMultipart {
+	if r.FormValue(`method`) == http.MethodPut {
 		a.CreateShare(w, r, config)
-	} else {
+	} else if r.FormValue(`method`) == http.MethodPost {
 		a.SaveFile(w, r, config)
+	} else {
+		a.renderer.Error(w, http.StatusMethodNotAllowed, errors.New(`Unknown method`))
 	}
 }
