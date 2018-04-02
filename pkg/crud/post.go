e5e5fff9cafbfae822b9e89bf97af3f5157fa427
Vincent Boutour <bob@vibioh.fr>
2018-04-02 20:50:18 +0200

    Adding rename

--- pkg/crud/post.go
+++ pkg/crud/post.go
@@ -19,6 +19,8 @@ func (a *App) Post(w http.ResponseWriter, r *http.Request, config *provider.Requ
 		}
 	} else if r.FormValue(`method`) == http.MethodPost {
 		a.SaveFiles(w, r, config)
+	} else if r.FormValue(`method`) == http.MethodPatch {
+		a.Rename(w, r, config)
 	} else if r.FormValue(`method`) == http.MethodPut {
 		a.CreateDir(w, r, config)
 	} else if r.FormValue(`method`) == http.MethodDelete {
--- pkg/crud/rename.go
+++ pkg/crud/rename.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/ViBiOh/fibr/pkg/provider"
+	"github.com/ViBiOh/fibr/pkg/utils"
 )
 
 // Rename rename given path to a new one
@@ -15,6 +17,11 @@ func (a *App) Rename(w http.ResponseWriter, r *http.Request, config *provider.Re
 	if !config.CanEdit {
 		a.renderer.Error(w, http.StatusForbidden, ErrNotAuthorized)
 		return
+	}
+
+	newName := r.FormValue(`newName`)
+	if strings.TrimSpace(newName) == `` {
+		a.renderer.Error(w, http.StatusBadRequest, errors.New(`New name is empty`))
 	}
 
 	filename, info, err := a.getFormOrPathFilename(r, config)
@@ -26,12 +33,16 @@ func (a *App) Rename(w http.ResponseWriter, r *http.Request, config *provider.Re
 		return
 	}
 
-	newFilename := r.URL.Query().Get(`newName`)
+	newFilename, newInfo := utils.GetPathInfo(a.rootDirectory, config.Root, config.Path, newName)
+	if newInfo != nil {
+		a.renderer.Error(w, http.StatusBadRequest, fmt.Errorf(`%s already exists`, newName))
+		return
+	}
 
 	if err := os.Rename(filename, newFilename); err != nil {
 		a.renderer.Error(w, http.StatusInternalServerError, fmt.Errorf(`Error while renaming file: %v`, err))
 		return
 	}
 
-	a.GetDir(w, config, path.Dir(newFilename), r.URL.Query().Get(`d`), &provider.Message{Level: `success`, Content: fmt.Sprintf(`%s successfully renamed to %s`, info.Name(), newFilename)})
+	a.GetDir(w, config, path.Dir(newFilename), r.URL.Query().Get(`d`), &provider.Message{Level: `success`, Content: fmt.Sprintf(`%s successfully renamed to %s`, info.Name(), newName)})
 }
