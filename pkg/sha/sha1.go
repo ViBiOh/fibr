c2f582a83c1f882d1a6442c23641cbc890bf9a8a
Vincent Boutour <bob@vibioh.fr>
2020-01-27 09:43:14 +0100

    fix: Taking care of sonar cloud advices

--- pkg/sha/sha1.go
+++ pkg/sha/sha1.go
@@ -4,13 +4,18 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"fmt"
+
+	"github.com/ViBiOh/httputils/v3/pkg/logger"
 )
 
 // Sha1 return SHA1 fingerprint
 func Sha1(o interface{}) string {
 	hasher := sha1.New()
+
 	// no err check https://golang.org/pkg/hash/#Hash
 	if _, err := hasher.Write([]byte(fmt.Sprintf("%#v", o))); err != nil {
+		logger.Error("%s", err)
+		return ""
 	}
 
 	return hex.EncodeToString(hasher.Sum(nil))
