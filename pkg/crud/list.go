5ad9bdd27077c716c5e8470143095e3955f19a9e
Vincent Boutour <bob@vibioh.fr>
2020-01-28 21:38:45 +0100

    feat: Allowing download of directory structure instead of flat

--- pkg/crud/list.go
+++ pkg/crud/list.go
@@ -50,14 +50,6 @@ func (a *app) List(w http.ResponseWriter, request provider.Request, message *pro
 
 // Download content of a directory into a streamed zip
 func (a *app) Download(w http.ResponseWriter, request provider.Request) {
-	files, err := a.storage.List(request.GetFilepath(""))
-	if err != nil {
-		a.renderer.Error(w, request, provider.NewError(http.StatusInternalServerError, err))
-		return
-	}
-
-	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s.zip", path.Base(request.Path)))
-
 	zipWriter := zip.NewWriter(w)
 	defer func() {
 		if err := zipWriter.Close(); err != nil {
@@ -65,25 +57,39 @@ func (a *app) Download(w http.ResponseWriter, request provider.Request) {
 		}
 	}()
 
-	for _, file := range files {
-		if file.IsDir {
-			continue
-		}
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s.zip", path.Base(request.Path)))
 
-		if err = a.addFileToZip(zipWriter, file); err != nil {
-			a.renderer.Error(w, request, provider.NewError(http.StatusInternalServerError, err))
-			return
-		}
+	if err := a.zipFiles(request, zipWriter, ""); err != nil {
+		a.renderer.Error(w, request, provider.NewError(http.StatusInternalServerError, err))
 	}
 }
 
-func (a *app) addFileToZip(zipWriter *zip.Writer, file provider.StorageItem) error {
+func (a *app) zipFiles(request provider.Request, zipWriter *zip.Writer, pathname string) error {
+	files, err := a.storage.List(request.GetFilepath(pathname))
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if file.IsDir {
+			if err := a.zipFiles(request, zipWriter, path.Join(pathname, file.Name)); err != nil {
+				return err
+			}
+		} else if err := a.addFileToZip(zipWriter, file, pathname); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *app) addFileToZip(zipWriter *zip.Writer, file provider.StorageItem, pathname string) error {
 	header, err := zip.FileInfoHeader(file.Info.(os.FileInfo))
 	if err != nil {
 		return err
 	}
 
-	header.Name = file.Name
+	header.Name = path.Join(pathname, file.Name)
 	header.Method = zip.Deflate
 
 	writer, err := zipWriter.CreateHeader(header)
