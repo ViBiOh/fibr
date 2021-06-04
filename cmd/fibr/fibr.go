265d125ae4f885f7066fea522816b14b4fb18fa9
Vincent Boutour <bob@vibioh.fr>
2021-06-04 11:52:40 +0200

    feat(pprof): Removing registered pprof listener
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/fibr/fibr.go
+++ cmd/fibr/fibr.go
@@ -3,8 +3,6 @@ package main
 import (
 	"embed"
 	"flag"
-	"net/http"
-	_ "net/http/pprof"
 	"os"
 
 	"github.com/ViBiOh/auth/v2/pkg/ident/basic"
@@ -43,7 +41,6 @@ func main() {
 
 	appServerConfig := server.Flags(fs, "", flags.NewOverride("ReadTimeout", "2m"), flags.NewOverride("WriteTimeout", "2m"))
 	promServerConfig := server.Flags(fs, "prometheus", flags.NewOverride("Port", 9090), flags.NewOverride("IdleTimeout", "10s"), flags.NewOverride("ShutdownTimeout", "5s"))
-	pprofServerConfig := server.Flags(fs, "pprof", flags.NewOverride("Port", 9999))
 	healthConfig := health.Flags(fs, "")
 
 	alcotestConfig := alcotest.Flags(fs, "")
@@ -70,7 +67,6 @@ func main() {
 
 	appServer := server.New(appServerConfig)
 	promServer := server.New(promServerConfig)
-	pprofServer := server.New(pprofServerConfig)
 	prometheusApp := prometheus.New(prometheusConfig)
 	healthApp := health.New(healthConfig)
 
@@ -100,7 +96,6 @@ func main() {
 	go metadataApp.Start(appServer.Done())
 	go crudApp.Start(appServer.Done())
 
-	go pprofServer.Start("pprof", healthApp.End(), http.DefaultServeMux)
 	go promServer.Start("prometheus", healthApp.End(), prometheusApp.Handler())
 	go appServer.Start("http", healthApp.End(), httputils.Handler(handler, healthApp, recoverer.Middleware, prometheusApp.Middleware, owasp.New(owaspConfig).Middleware))
 
