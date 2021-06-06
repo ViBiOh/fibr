2687d5a6f71aca0543d3291ad2d028d0246f09cf
Vincent Boutour <bob@vibioh.fr>
2021-06-06 17:56:01 +0200

    feat(fibr): Adding sitemap renderingCloses None
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/fibr/renderer.go
+++ pkg/fibr/renderer.go
@@ -62,6 +62,10 @@ func (a app) TemplateFunc(w http.ResponseWriter, r *http.Request) (string, int, 
 		return "", 0, nil, model.WrapMethodNotAllowed(errors.New("you lack of method for calling me"))
 	}
 
+	if r.URL.Path == "/sitemap.xml" {
+		return "sitemap", http.StatusOK, nil, nil
+	}
+
 	if query.GetBool(r, "redirect") {
 		a.rendererApp.Redirect(w, r, r.URL.Path, renderer.ParseMessage(r))
 		return "", 0, nil, nil
