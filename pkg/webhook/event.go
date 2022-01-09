891f02083d26056d49718943243de2bc0d489065
Vincent Boutour <bob@vibioh.fr>
2022-01-09 13:26:09 +0100

    fix: Increasing the wait
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/webhook/event.go
+++ pkg/webhook/event.go
@@ -106,7 +106,7 @@ func (a *App) discordHandle(ctx context.Context, webhook provider.Webhook, event
 		}
 	}
 
-	time.Sleep(10)
+	time.Sleep(time.Second * 10)
 
 	return send(ctx, webhook.ID, request.Post(webhook.URL), discordPayload{
 		Embeds: []discordEmbed{embed},
