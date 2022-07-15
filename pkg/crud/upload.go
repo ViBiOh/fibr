a296fd017832a760fadc1b323c3f3e49d4fd8cb4
Vincent Boutour <bob@vibioh.fr>
2022-07-15 16:12:51 +0200

    feat(chunk): Starting to add chunk uploader
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/crud/upload.go
+++ pkg/crud/upload.go
@@ -15,10 +15,12 @@ import (
 	"github.com/ViBiOh/httputils/v4/pkg/renderer"
 )
 
-func (a App) saveUploadedFile(ctx context.Context, request provider.Request, inputName, rawSize string, part *multipart.Part) (filename string, err error) {
-	var filepath string
+const temporaryFolder = "/tmp"
 
-	filename, filepath, err = getUploadNameAndPath(request, inputName, part)
+func (a App) saveUploadedFile(ctx context.Context, request provider.Request, inputName, rawSize string, file *multipart.Part) (fileName string, err error) {
+	var filePath string
+
+	fileName, filePath, err = getUploadNameAndPath(request, inputName, file)
 	if err != nil {
 		return "", fmt.Errorf("unable to get upload name: %s", err)
 	}
@@ -29,37 +31,37 @@ func (a App) saveUploadedFile(ctx context.Context, request provider.Request, inp
 		return "", fmt.Errorf("unable to get upload size: %s", err)
 	}
 
-	err = provider.WriteToStorage(ctx, a.storageApp, filepath, size, part)
+	err = provider.WriteToStorage(ctx, a.storageApp, filePath, size, file)
 
 	if err == nil {
 		go func() {
-			if info, infoErr := a.storageApp.Info(context.Background(), filepath); infoErr != nil {
+			if info, infoErr := a.storageApp.Info(context.Background(), filePath); infoErr != nil {
 				logger.Error("unable to get info for upload event: %s", infoErr)
 			} else {
-				a.notify(provider.NewUploadEvent(request, info, a.bestSharePath(filepath), a.rendererApp))
+				a.notify(provider.NewUploadEvent(request, info, a.bestSharePath(filePath), a.rendererApp))
 			}
 		}()
 	}
 
-	return filename, err
+	return fileName, err
 }
 
-func getUploadNameAndPath(request provider.Request, inputName string, part *multipart.Part) (filename string, filepath string, err error) {
+func getUploadNameAndPath(request provider.Request, inputName string, part *multipart.Part) (fileName string, filePath string, err error) {
 	if !request.Share.IsZero() && request.Share.File {
 		return path.Base(request.Share.Path), request.Share.Path, nil
 	}
 
 	if len(inputName) != 0 {
-		filename = inputName
+		fileName = inputName
 	} else {
-		filename = part.FileName()
+		fileName = part.FileName()
 	}
 
-	filename, err = provider.SanitizeName(filename, true)
+	fileName, err = provider.SanitizeName(fileName, true)
 	if err != nil {
 		return
 	}
-	filepath = request.SubPath(filename)
+	filePath = request.SubPath(fileName)
 
 	return
 }
@@ -77,13 +79,13 @@ func getUploadSize(rawSize string) (int64, error) {
 }
 
 // Upload saves form files to filesystem
-func (a App) Upload(w http.ResponseWriter, r *http.Request, request provider.Request, values map[string]string, part *multipart.Part) {
+func (a App) Upload(w http.ResponseWriter, r *http.Request, request provider.Request, values map[string]string, file *multipart.Part) {
 	if !request.CanEdit {
 		a.error(w, r, request, model.WrapForbidden(ErrNotAuthorized))
 		return
 	}
 
-	if part == nil {
+	if file == nil {
 		a.error(w, r, request, model.WrapInvalid(errors.New("no file provided for save")))
 		return
 	}
@@ -102,7 +104,7 @@ func (a App) Upload(w http.ResponseWriter, r *http.Request, request provider.Req
 
 	ctx := r.Context()
 
-	filename, err := a.saveUploadedFile(ctx, request, values["filename"], values["size"], part)
+	filename, err := a.saveUploadedFile(ctx, request, values["filename"], values["size"], file)
 	if err != nil {
 		a.error(w, r, request, model.WrapInternal(err))
 		return
