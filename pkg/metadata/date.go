a5cab899d6a4a7bb9793f85e0d8834facbafa1a2
Vincent Boutour <bob@vibioh.fr>
2023-01-29 18:17:20 +0100

    feat: Adding debug log for update date
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/metadata/date.go
+++ pkg/metadata/date.go
@@ -6,10 +6,15 @@ import (
 
 	absto "github.com/ViBiOh/absto/pkg/model"
 	"github.com/ViBiOh/fibr/pkg/provider"
+	"github.com/ViBiOh/httputils/v4/pkg/logger"
 )
 
 func (a App) updateDate(ctx context.Context, item absto.Item, data provider.Metadata) error {
 	if data.Date.IsZero() || item.Date.Equal(data.Date) {
+		logger.WithField("item", item.Pathname).
+			WithField("item_date", item.Date.String()).
+			WithField("exif_date", data.Date.String()).
+			Debug("no exif date or already equal")
 		return nil
 	}
 
