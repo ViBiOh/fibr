f2fb0ff99b71970c799cc7dc189c9e67f897f228
Vincent Boutour <bob@vibioh.fr>
2023-09-02 09:44:55 +0200

    fix: Add signal to listen for termination
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/fibr/fibr.go
+++ cmd/fibr/fibr.go
@@ -8,6 +8,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"syscall"
 
 	_ "net/http/pprof"
 
@@ -84,5 +85,5 @@ func main() {
 
 	go adapters.eventBus.Start(endCtx, adapters.storage, []provider.Renamer{services.thumbnail.Rename, services.metadata.Rename}, services.share.EventConsumer, services.thumbnail.EventConsumer, services.metadata.EventConsumer, services.webhook.EventConsumer)
 
-	clients.health.WaitForTermination(ports.TerminateOnDone())
+	clients.health.WaitForTermination(ports.TerminateOnDone(), syscall.SIGTERM)
 }
