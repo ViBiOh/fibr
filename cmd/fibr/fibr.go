b229d003d46035f8c25f76baa66b531d6cd79962
Vincent Boutour <bob@vibioh.fr>
2023-12-26 22:24:45 +0100

    refactor(logger): Factorizing way to fatal if err
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/fibr/fibr.go
+++ cmd/fibr/fibr.go
@@ -17,6 +17,7 @@ import (
 	"github.com/ViBiOh/fibr/pkg/provider"
 	"github.com/ViBiOh/httputils/v4/pkg/alcotest"
 	"github.com/ViBiOh/httputils/v4/pkg/httputils"
+	"github.com/ViBiOh/httputils/v4/pkg/logger"
 	"github.com/ViBiOh/httputils/v4/pkg/owasp"
 	"github.com/ViBiOh/httputils/v4/pkg/recoverer"
 	"github.com/ViBiOh/httputils/v4/pkg/server"
@@ -52,26 +53,17 @@ func main() {
 	ctx := context.Background()
 
 	clients, err := newClient(ctx, config)
-	if err != nil {
-		slog.ErrorContext(ctx, "clients", "error", err)
-		os.Exit(1)
-	}
+	logger.FatalfOnErr(ctx, err, "clients")
 
 	defer clients.Close(ctx)
 
 	adapters, err := newAdapters(config, clients)
-	if err != nil {
-		slog.ErrorContext(ctx, "adapters", "error", err)
-		os.Exit(1)
-	}
+	logger.FatalfOnErr(ctx, err, "adapters")
 
 	endCtx := clients.health.EndCtx()
 
 	services, err := newServices(endCtx, config, clients, adapters)
-	if err != nil {
-		slog.ErrorContext(ctx, "services", "error", err)
-		os.Exit(1)
-	}
+	logger.FatalfOnErr(ctx, err, "services")
 
 	stopOnDone := Starters{services.amqpThumbnail, services.amqpExif, services.sanitizer}
 	stopOnDone.Start(clients.health.DoneCtx())
