a3c8ce6e491dd2bba82a1964aebd00b891d610ea
Vincent Boutour <bob@vibioh.fr>
2024-07-28 20:36:56 +0200

    chore(deps): Bumping pdf-dist
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/fibr/static/scripts/pdf.mjs
+++ cmd/fibr/static/scripts/pdf.mjs
@@ -1,7 +1,7 @@
-import * as pdfjs from "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.4.168/+esm";
+import * as pdfjs from "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.5.136/+esm";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.4.168/build/pdf.worker.min.mjs",
+  "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.5.136/build/pdf.worker.min.mjs",
   import.meta.url,
 ).toString();
 
