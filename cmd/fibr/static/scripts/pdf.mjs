a3da8f25d86ccc44ab0b71fd374eb0a7fbf5a0c5
Vincent Boutour <bob@vibioh.fr>
2024-12-05 20:52:30 +0100

    chore(deps): Bumping deps
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/fibr/static/scripts/pdf.mjs
+++ cmd/fibr/static/scripts/pdf.mjs
@@ -1,7 +1,7 @@
-import * as pdfjs from "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.8.69/+esm";
+import * as pdfjs from "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.9.124/+esm";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.8.69/build/pdf.worker.min.mjs",
+  "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.9.124/build/pdf.worker.min.mjs",
   import.meta.url,
 ).toString();
 
