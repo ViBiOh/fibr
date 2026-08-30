07056273dd4afb23deb0333a958c1dc27d15a56a
Vincent Boutour <bob@vibioh.fr>
2026-08-30 12:33:30 +0200

    chore(deps): Bumping pdfjs version
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/fibr/static/scripts/pdf.mjs
+++ cmd/fibr/static/scripts/pdf.mjs
@@ -1,7 +1,7 @@
-import * as pdfjs from "https://cdn.jsdelivr.net/npm/pdfjs-dist@6.2.108/build/pdf.min.mjs";
+import * as pdfjs from "https://cdn.jsdelivr.net/npm/pdfjs-dist@6.3.289/build/pdf.min.mjs";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "https://cdn.jsdelivr.net/npm/pdfjs-dist@6.2.108/build/pdf.worker.min.mjs",
+  "https://cdn.jsdelivr.net/npm/pdfjs-dist@6.3.289/build/pdf.worker.min.mjs",
   import.meta.url,
 ).toString();
 
